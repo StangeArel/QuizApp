304c26344bc21ae11e6ac70d1fcd1c35fd1154e7
Valeriya Stange <>
2025-01-20 21:40:38 +0100

    Endscreen erstellen

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ let questions = [
     },
     {
         "question": "Welcher Dinosaurier hatte Hörner und ein großes Schild um seinen Kopf?",
-        "question_1": "Tyrannosaurus Rex",
-        "question_2": "Triceratops",
+        "question_1": "Triceratops",
+        "question_2": "Tyrannosaurus Rex",
         "question_3": "Brachiosaurus",
         "question_4": "Velociraptor",
-        "right_answer": 2
+        "right_answer": 1
     },
     {
         "question": "Welcher Dino war dafür bekannt, einen 'Panzer' zur Verteidigung zu haben?",
@@ -35,10 +35,10 @@ let questions = [
         "question": "Wie lange dauerte die Herrschaft der Dinosaurier auf der Erde?",
         "question_1": "Etwa 65 Millionen Jahre",
         "question_2": "Etwa 100 Millionen Jahre",
-        "question_3": "Etwa 165 Millionen Jahre",
-        "question_4": "Etwa 200 Millionen Jahre",
-        "right_answer": 3
-    },
+        "question_3": "Etwa 200 Millionen Jahre",
+        "question_4": "Etwa 165 Millionen Jahre",
+        "right_answer": 4
+    }
 ];
 
 let currentQuestion = 0;
@@ -49,13 +49,19 @@ function init() {
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion]; // let question = questions[0]
-
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['question_1'];
-    document.getElementById('answer_2').innerHTML = question['question_2'];
-    document.getElementById('answer_3').innerHTML = question['question_3'];
-    document.getElementById('answer_4').innerHTML = question['question_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = "";
+        document.getElementById('questionBody').style = "display: none";
+    } else {
+        let question = questions[currentQuestion]; // let question = questions[0]
+        /*     document.getElementById('nextButton').disabled = true; // Button deaktivieren */
+        document.getElementById("this-questions").innerHTML = currentQuestion + 1;
+        document.getElementById('questionText').innerHTML = question['question']; // question.question
+        document.getElementById('answer_1').innerHTML = question['question_1'];   // question.question_1
+        document.getElementById('answer_2').innerHTML = question['question_2'];
+        document.getElementById('answer_3').innerHTML = question['question_3'];
+        document.getElementById('answer_4').innerHTML = question['question_4'];
+    }
 }
 
 function answer(selection) {
@@ -64,11 +70,28 @@ function answer(selection) {
     let idOfRightAnswer = /* `answer_2` */ `answer_${question.right_answer}`;
 
     if (SelectedQuestionNumber == question.right_answer) {
-        console.log('Wow! Richtige Antwort!');
         document.getElementById(selection).parentNode.classList.add('bg-success');
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('nextButton').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('nextButton').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
